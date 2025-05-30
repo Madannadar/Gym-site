c20a7f5a0c1596f78e550d22b828cddb1b3cadfe
pavandhadge <dhadgepavan00@gmail.com>
2025-05-30 15:49:03 +0530

    added user and event

--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -1,8 +1,26 @@
-import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { Router } from "express";
+import {
+  handleCreateUser,
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateName,
+  handleUpdateVegetarian,
+  handleUpdatePassword,
+  handleUpdateSubscription,
+  handleDeleteUser,
+} from "../controllers/user.controller.js";
 
-const router = express.Router();
+const router = Router();
 
-router.post("/addUser", createUser);
+router.post("/users", handleCreateUser);
+router.get("/users", handleGetAllUsers);
+router.get("/users/:user_id", handleGetUserById);
+
+router.put("/users/:user_id/name", handleUpdateName);
+router.put("/users/:user_id/vegetarian", handleUpdateVegetarian);
+router.put("/users/:user_id/password", handleUpdatePassword);
+router.put("/users/:user_id/subscription", handleUpdateSubscription);
+
+router.delete("/users/:user_id", handleDeleteUser);
 
 export default router;
