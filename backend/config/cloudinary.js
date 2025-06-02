d78bf2776b371ab12b76115473e76552b883bddf
tanmay-exits <tanmayrewale232021@gmail.com>
2025-06-02 17:25:02 +0530

    .

--- backend/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import cloudinary from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME || 'your-cloud-name',
-  api_key: process.env.CLOUD_API_KEY || 'your-cloud-api-key',
-  api_secret: process.env.CLOUD_API_SECRET || 'your-cloud-api-secret',
-});
-
-export default cloudinary;
